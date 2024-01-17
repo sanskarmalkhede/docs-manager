c5fdd9e896660b42c62581726d5791eb2bc91da9
Sanskar M <malkhedesanskar@gmail.com>
2024-01-18 02:15:32 +0530

    Created Components and update title and add new dependencies

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <>
+            <div className='w-60 h-80 rounded-[1.25rem]  bg-zinc-900/80'></div>
+            //icon
+            //text
+            //footer
+            //progress bar
+        </>
+    )
+}
+
+export default Card
