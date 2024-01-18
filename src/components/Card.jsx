70cc983bf882685d3521255899317b293324fb74
Sanskar M <malkhedesanskar@gmail.com>
2024-01-18 15:56:21 +0530

    Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { IoCloseSharp } from "react-icons/io5";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+
 function Card({ data, reference }) {
   return (
     <>
-      <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.1}} dragElastic={0.2} className="relative flex-shrink-0 w-[18rem] h-[22rem] rounded-[3.2rem]  bg-zinc-900/80 p-7 text-white overflow-hidden">
+      <motion.div
+        drag
+        dragConstraints={reference}
+        whileDrag={{ scale: 1.1 }}
+        dragElastic={0.2}
+        className="relative flex-shrink-0 w-[18rem] h-[22rem] rounded-[3.2rem]  bg-zinc-900/80 p-7 text-white overflow-hidden"
+      >
         <FaRegFileAlt />
-        <p className="text-sm font-sans font-medium mt-5 font-normal ">
-          {data.description}
-        </p>
+        <p className="text-sm font-sans font-medium mt-5">{data.description}</p>
         <div className="footer w-full left-0 absolute bottom-0">
           <div className=" flex items-center justify-between px-8 py-3 mb-3">
             <h5>{data.filesize}</h5>
